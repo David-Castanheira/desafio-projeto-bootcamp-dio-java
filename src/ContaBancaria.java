f7874c802feb2a91605f2ac809a28544bd1a77d5
David Castanheira <david.castanheira@aluno.ifsp.edu.br>
2025-06-19 10:35:11 -0300

    Refactor: Refatorando a classe ContaBancaria, tornando-a mais legível

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -5,15 +5,20 @@ public class ContaBancaria {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite o número da agência");
-        var agencia = sc.nextInt();
+        int agencia = sc.nextInt();
+
         System.out.println("Digite o número da conta");
-        var conta = sc.next();
+        String conta = sc.next();
+
         System.out.println("Informe o nome do cliente");
-        var nomeCliente = sc.next();
+        String nomeCliente = sc.next();
+
         System.out.println("Informe o saldo da conta");
-        var saldo = sc.nextDouble();
+        double saldo = sc.nextDouble();
 
         System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, " +
                 "conta %s e seu saldo %s já está disponível para saque", nomeCliente, agencia, conta, saldo);
+
+        sc.close();
     }
 }
